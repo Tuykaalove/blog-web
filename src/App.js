e0ed90b1e72a5f8bcfce572136d6726197e97440
Evening <evening@admins-iMac.local>
2023-01-05 17:44:53 +0800

    categories

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
 //import logo from './logo.svg';
-import 'react-awesome-button/dist/styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AwesomeButton } from 'react-awesome-button';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import './App.css';
-import { Component } from 'react';
-import { CategoryList } from './Component/CategoryList';
-
-
-const containerStyle = {
-  backgroundColor: 'blue',
-  fontSize: '50px',
-  
-}
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-awesome-button/dist/styles.css";
+import Container from "react-bootstrap/Container";
+import { CategoryList } from './Component/CategoriesList';
+import { Categories } from './Component/Categories';
 
 
 function App() {
   const name = "sarnai";
   return (
     <>
-    
-            <div className="App" style={containerStyle}>Hello
-            </div>
-            <div>
-              <Container className="justify-content-md-center">
-                <Navbar> 
-                  <Nav>
-                  <Nav.Link href="#">Админ</Nav.Link>
-                  <Nav.Link href="#">Хэрэглэгч</Nav.Link>
-                  <Nav.Link href="#">Ангилал</Nav.Link>
-                  <Nav.Link href="#">Мэдээ</Nav.Link>
-                  <Nav.Link href="#">Сэдэв</Nav.Link>
-                  </Nav>
-             
-              </Navbar>
-                    Ангилал
-                    <CategoryList/>
-                 
+    <Container>
+      <Categories/>
+    </Container>
             
-               </Container>
-            </div>
+            
            
            
            
